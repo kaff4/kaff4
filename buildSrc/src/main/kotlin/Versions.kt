aae13643cfa5a1365520288cbbb92b4bf16b91a6
Kevin Brightwell <kevin.brightwell2@gmail.com>
2023-03-12 13:18:11 -0400

    Upgrade Gradle to 7.6.1 (#26)

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,5 +1,9 @@
+import org.gradle.jvm.toolchain.JavaLanguageVersion
+import org.gradle.jvm.toolchain.JavaToolchainSpec
+import org.gradle.jvm.toolchain.JvmVendorSpec
+
 object Versions {
-  const val JVM_BYTECODE_TARGET = "18"
+  const val JVM_BYTECODE_TARGET = 19
   const val KOTLIN = "1.8.10"
   const val DETEKT = "1.22.0"
 
@@ -10,5 +14,11 @@ object Versions {
 
   const val LOG4J = "2.17.1"
   const val RDF4J = "4.0.0"
+
+
+  fun JavaToolchainSpec.configureJavaToolchain() {
+    languageVersion.set(JavaLanguageVersion.of(Versions.JVM_BYTECODE_TARGET))
+    vendor.set(JvmVendorSpec.ADOPTIUM)
+  }
 }
 
