483bae366ee2aac95a2b9cde49fd6c531b72bd01
Kevin Brightwell <kevin.brightwell2@gmail.com>
2023-03-19 15:47:42 -0400

    Fix Java Version (#30)
    Due to google/guice#1654 Java 17 is the max version supported.

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -3,7 +3,7 @@ import org.gradle.jvm.toolchain.JavaToolchainSpec
 import org.gradle.jvm.toolchain.JvmVendorSpec
 
 object Versions {
-  const val JVM_BYTECODE_TARGET = 19
+  const val JVM_BYTECODE_TARGET = 17
   const val KOTLIN = "1.8.10"
   const val DETEKT = "1.22.0"
 
