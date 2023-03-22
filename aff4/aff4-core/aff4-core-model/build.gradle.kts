4c674dcdb94ee538eb1d4206fa050edf17407f6f
Kevin Brightwell <kevin.brightwell2@gmail.com>
2023-03-22 12:02:22 -0400

    deps

--- aff4/aff4-core/aff4-core-model/build.gradle.kts
+++ aff4/aff4-core/aff4-core-model/build.gradle.kts
@@ -1,19 +1,17 @@
 dependencies {
   compileOnly(kotlin("reflect"))
 
+  api(project(":aff4:aff4-core:aff4-core-guice"))
   api(project(":aff4:aff4-core:aff4-core-model:aff4-core-model-api"))
   api(project(":aff4:aff4-core:aff4-core-okio"))
   api(project(":aff4:aff4-plugin"))
   api(project(":aff4:aff4-rdf"))
 
   api(Dependencies.GUICE)
+  api(Dependencies.JAVAX_INJECT)
   api(Dependencies.OKIO)
   api(Dependencies.RDF4J_MODEL_API)
-
-  implementation(Dependencies.JAVAX_INJECT)
-
-  implementation(project(":aff4:aff4-core:aff4-core-guice"))
-
+  
   testImplementation(Dependencies.RDF4J_MODEL)
 
   testImplementation(project(":aff4:aff4-core:aff4-core-test"))
