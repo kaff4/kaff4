6bec1caaecc575629008950278ae5d08db881495
Kevin Brightwell <kevin.brightwell2@gmail.com>
2023-03-22 12:03:55 -0400

    [Test] Add `TestLifecycleAction` to test to enable adding `ActionScop… (#45)
    …e` for future tests
    
    This removes the `CleanupActions` in favour of using dedicated
    `TestLifecycleAction` implementations.
    
    Extracted from #42

--- aff4/aff4-core/aff4-core-test/src/main/kotlin/com/github/nava2/aff4/Aff4ImageTestModule.kt
+++ aff4/aff4-core/aff4-core-test/src/main/kotlin/com/github/nava2/aff4/Aff4ImageTestModule.kt
@@ -8,15 +8,22 @@ import com.github.nava2.test.GuiceExtension
 import com.google.inject.Provides
 import okio.FileSystem
 import okio.Path.Companion.toPath
+import javax.inject.Inject
 import javax.inject.Singleton
 
 abstract class Aff4ImageTestModule(val imageName: String) : KAbstractModule() {
+  val imagePath = imageName.toPath()
+
   override fun configure() {
     install(Aff4TestModule)
 
     install(Aff4ImageOpenerModule)
     install(Aff4CoreModule)
     install(Aff4BaseStreamModule)
+
+    bindSet<GuiceExtension.TestLifecycleAction> {
+      to<ImageTestLifecycleAction>()
+    }
 
     configureOther()
   }
@@ -26,29 +33,47 @@ abstract class Aff4ImageTestModule(val imageName: String) : KAbstractModule() {
   @Provides
   @UnderTest
   @Singleton
-  internal fun providesAff4ContainerUnderTest(
-    aff4ImageOpener: Aff4ImageOpener,
-    cleanupActions: GuiceExtension.CleanupActions,
-    @ForImages imagesFileSystem: FileSystem,
-  ): Aff4Image {
-    check(imagesFileSystem.exists(imageName.toPath())) {
-      "Image path does not exist: ${imagesFileSystem.canonicalize(imageName.toPath())}"
-    }
-
-    return aff4ImageOpener.manualOpen(imagesFileSystem, imageName.toPath())
-      .apply(cleanupActions::register)
-      .aff4Image
+  internal fun providesAff4ContainerUnderTest(imageTestLifecycleAction: ImageTestLifecycleAction): Aff4Image {
+    return imageTestLifecycleAction.openImage(imageName)
   }
 
   @Provides
   @Singleton
   @UnderTest
-  internal fun providesAff4ModelUnderTest(@UnderTest aff4Image: Aff4Image) = aff4Image.aff4Model
+  internal fun providesAff4ModelUnderTest(@UnderTest image: Aff4Image) = image.aff4Model
 
   @Provides
   @Singleton
   @UnderTest
-  internal fun providesAff4StreamOpenerUnderTest(@UnderTest aff4Image: Aff4Image) = aff4Image.streamOpener
+  internal fun providesAff4StreamOpenerUnderTest(@UnderTest image: Aff4Image) = image.streamOpener
+
+  @Singleton
+  internal class ImageTestLifecycleAction @Inject constructor(
+    private val aff4ImageOpener: Aff4ImageOpener,
+    @ForImages private val imagesFileSystem: FileSystem,
+  ) : GuiceExtension.TestLifecycleAction {
+
+    private var imageWithResources: Aff4ImageOpener.Aff4ImageWithResources? = null
+
+    @Synchronized
+    fun openImage(imageName: String): Aff4Image {
+      check(imageWithResources == null) { "Can not open multiple images" }
+
+      check(imagesFileSystem.exists(imageName.toPath())) {
+        "Image path does not exist: ${imagesFileSystem.canonicalize(imageName.toPath())}"
+      }
+
+      val openedImageWithResources = aff4ImageOpener.manualOpen(imagesFileSystem, imageName.toPath())
+      imageWithResources = openedImageWithResources
+      return openedImageWithResources.aff4Image
+    }
+
+    @Synchronized
+    override fun afterEach() {
+      imageWithResources?.close()
+      imageWithResources = null
+    }
+  }
 
   companion object
 }
--- aff4/aff4-core/src/test/kotlin/com/github/nava2/aff4/Aff4ImageTestModuleDreamExtension.kt
+++ aff4/aff4-core/src/test/kotlin/com/github/nava2/aff4/Aff4ImageTestModuleDreamExtension.kt
@@ -1,43 +1,14 @@
 package com.github.nava2.aff4
 
-import com.github.nava2.aff4.model.Aff4Container
-import com.github.nava2.aff4.model.dialect.DefaultToolDialect
-import com.github.nava2.aff4.model.dialect.DialectTypeResolver
-import com.github.nava2.aff4.model.dialect.ToolDialect
-import com.github.nava2.aff4.model.rdf.ZipSegment
+import com.github.nava2.aff4.dialects.Pyaff4Version11ToolDialect
 import com.github.nava2.aff4.streams.compression.Aff4SnappyPlugin
 import com.google.inject.Module
-import com.google.inject.Provides
-import javax.inject.Singleton
 
-val Aff4ImageTestModule.Companion.Dream: Module
+val Aff4ImageTestModule.Companion.Dream: Aff4ImageTestModule
   get() = object : Aff4ImageTestModule(imageName = "dream.aff4") {
     override fun configureOther() {
       install(Aff4SnappyPlugin)
       install(Aff4LogicalModule)
-
-      bindSet<ToolDialect> {
-        to<DreamToolDialect>()
-      }
-    }
-
-    @Provides
-    @Singleton
-    internal fun providesDreamToolDialect(
-      @DefaultToolDialect defaultToolDialect: ToolDialect,
-    ): DreamToolDialect {
-      val dreamDialectTypeResolver = DialectTypeResolver.Builder.withExisting(defaultToolDialect.typeResolver)
-        .apply {
-          remove(ZipSegment::class)
-          register(ZipSegment::class, "aff4:zip_segment")
-        }
-        .build()
-      return DreamToolDialect(dreamDialectTypeResolver)
+      install(Pyaff4Version11ToolDialect.Module)
     }
   }
-
-private class DreamToolDialect(override val typeResolver: DialectTypeResolver) : ToolDialect {
-  override fun isApplicable(toolMetadata: Aff4Container.ToolMetadata): Boolean {
-    return toolMetadata == Aff4Container.ToolMetadata(version = "1.1", tool = "pyaff4")
-  }
-}
