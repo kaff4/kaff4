a1a043f8951891449f938cab188d3afe2a4eb3e2
Kevin Brightwell <kevin.brightwell2@gmail.com>
2023-04-04 07:50:16 -0400

    Add missing `@ActionScoped` annotations

--- aff4/aff4-core/src/main/kotlin/com/github/nava2/aff4/container/Aff4ImageOpenerModule.kt
+++ aff4/aff4-core/src/main/kotlin/com/github/nava2/aff4/container/Aff4ImageOpenerModule.kt
@@ -41,7 +41,7 @@ object Aff4ImageOpenerModule : KAff4AbstractModule() {
   }
 
   private class Aff4ImageActionScopedProvider @Inject constructor(
-    private val aff4ModelProvider: Provider<Aff4Model>,
+    @ActionScoped private val aff4ModelProvider: Provider<Aff4Model>,
     private val streamOpener: Aff4StreamOpener,
     private val loadedContainersContextProvider: Provider<RealAff4ImageOpener.LoadedContainersContext>,
   ) : Provider<Aff4Image> {
@@ -54,7 +54,7 @@ object Aff4ImageOpenerModule : KAff4AbstractModule() {
 
   private class Aff4ImageContextActionScopedProvider @Inject constructor(
     private val rdfExecutor: RdfExecutor,
-    private val loadedContainersContextProvider: Provider<RealAff4ImageOpener.LoadedContainersContext>,
+    @ActionScoped private val loadedContainersContextProvider: Provider<RealAff4ImageOpener.LoadedContainersContext>,
   ) : Provider<Aff4ImageContext> {
     override fun get(): Aff4ImageContext {
       val loadedContainersContext = loadedContainersContextProvider.get()
@@ -68,13 +68,13 @@ object Aff4ImageOpenerModule : KAff4AbstractModule() {
 
   private class Aff4ModelActionScopedProvider @Inject constructor(
     private val aff4ModelLoader: Aff4Model.Loader,
-    private val aff4ImageContextProvider: Provider<Aff4ImageContext>,
+    @ActionScoped private val aff4ImageContextProvider: Provider<Aff4ImageContext>,
   ) : Provider<Aff4Model> {
     override fun get() = aff4ModelLoader.load(aff4ImageContextProvider.get())
   }
 
   private class Aff4ToolDialectActionScopedProvider @Inject constructor(
-    private val loadedContainersContextProvider: Provider<RealAff4ImageOpener.LoadedContainersContext>,
+    @ActionScoped private val loadedContainersContextProvider: Provider<RealAff4ImageOpener.LoadedContainersContext>,
     private val toolDialectResolver: ToolDialectResolver,
   ) : Provider<ToolDialect> {
     override fun get(): ToolDialect {
