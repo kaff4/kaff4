4cd552069b149a406e0943ef1fa983719d3f47aa
Kevin Brightwell <kevin.brightwell2@gmail.com>
2023-04-07 10:40:37 -0400

    fix ordering of settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,20 +1,20 @@
 rootProject.name = "kaff4"
 
 include(
+  "kaff4-cli",
   "kaff4-compression:kaff4-compression-lz4",
   "kaff4-compression:kaff4-compression-snappy",
   "kaff4-compression:kaff4-compression-test",
   "kaff4-core",
+  "kaff4-core:kaff4-core-guice",
   "kaff4-core:kaff4-core-kotlin",
+  "kaff4-core:kaff4-core-logging",
   "kaff4-core:kaff4-core-model",
   "kaff4-core:kaff4-core-model:kaff4-core-model-api",
   "kaff4-core:kaff4-core-okio",
   "kaff4-core:kaff4-core-test",
-  "kaff4-core:kaff4-core-guice",
-  "kaff4-core:kaff4-core-logging",
   "kaff4-plugin",
   "kaff4-rdf",
   "kaff4-rdf:kaff4-rdf-api",
   "kaff4-rdf:kaff4-rdf-memory",
-  "kaff4-cli",
 )
