e57e358894b0548f6758cfb2a542b5a7b6886d90
Kevin Brightwell <kevin.brightwell2@gmail.com>
2023-06-02 16:20:12 -0400

    Appease buildHealth

--- kaff4-core/kaff4-core-okio/build.gradle.kts
+++ kaff4-core/kaff4-core-okio/build.gradle.kts
@@ -3,6 +3,7 @@ dependencies {
 
   implementation(Dependencies.OKIO)
 
+  testImplementation(project(":kaff4-core:kaff4-core-okio"))
   testImplementation(project(":kaff4-core:kaff4-core-test"))
 }
 
