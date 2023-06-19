a3dcffe4f2313787813fa9bd5f48e295747788fb
Kevin Brightwell <kevin.brightwell2@gmail.com>
2023-06-19 15:47:38 -0400

    Adjust down to 4.0.5

--- kaff4-rdf/kaff4-rdf-api/build.gradle.kts
+++ kaff4-rdf/kaff4-rdf-api/build.gradle.kts
@@ -2,6 +2,6 @@ dependencies {
   api("com.google.inject:guice:5.1.0")
   api("javax.inject:javax.inject:1")
 
-  api("org.eclipse.rdf4j:rdf4j-model-api:4.3.2")
-  api("org.eclipse.rdf4j:rdf4j-sail-api:4.3.2")
+  api("org.eclipse.rdf4j:rdf4j-model-api:4.0.5")
+  api("org.eclipse.rdf4j:rdf4j-sail-api:4.0.5")
 }
